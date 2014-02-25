fc302b8b63d9d726a5498d72ef1172f835323d8a
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-02-24 21:08:26 -0500

    update timer static variable names

--- src/server/controllers/SimulationTimer.java
+++ src/server/controllers/SimulationTimer.java
@@ -8,14 +8,14 @@ public class SimulationTimer extends TimerTask {
 	
 	public static final int
 		MS_IN_S = 1000, 
-		SIMULATION_UPDATE_FREQ = 60,
-		SIMULATION_UPDATE_DELAY = MS_IN_S / SIMULATION_UPDATE_FREQ;
+		UPDATE_FREQ = 60,
+		UPDATE_DELAY = MS_IN_S / UPDATE_FREQ;
 	
 	private final Server server;
 
 	public SimulationTimer(Server server) {
 		this.server = server;
-		new Timer().scheduleAtFixedRate(this, 0, SIMULATION_UPDATE_DELAY);
+		new Timer().scheduleAtFixedRate(this, 0, UPDATE_DELAY);
 	}
 	@Override
 	public void run() {
