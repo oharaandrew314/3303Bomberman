8ed0f9908f30e7c9614c10c70e4a2315e1b6eb68
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-01 18:09:37 -0500

    Server to interpret move commands

--- src/server/controllers/Server.java
+++ src/server/controllers/Server.java
@@ -1,25 +1,89 @@
 package server.controllers;
 
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
+
 import common.controllers.GameController;
+import common.events.ConnectEvent;
 import common.events.Event;
+import common.events.GameKeyEvent;
+import common.events.ViewUpdateEvent;
+import common.models.Player;
 
 public class Server extends GameController {
+	
+	public static final int MAX_PLAYERS = 4;
+	private Map<Integer, Player> players;
+	
+	@Deprecated
+	private static final int DUMMY_ID = 1;
 
 	public Server() {
 		new SimulationTimer(this);
+		players = new HashMap<>();
 	}
 	
-	public void simulationUpdate(){
-		//TODO: Implement
-	}
-	
-	public static void main(String[] args){
-		new Server();
+	public synchronized void simulationUpdate(){
+		//TODO: Bomb logic
+		//TODO: AI logic
+		
+		nwc.send(new ViewUpdateEvent(grid));
 	}
 
     @Override
-    public void receive(Event event) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public synchronized void receive(Event event) {
+    	// Accept ConntectEvent and add player to game
+    	if (event instanceof ConnectEvent){
+    		if (players.size() < MAX_PLAYERS){
+    			players.put(DUMMY_ID, new Player("Player " + DUMMY_ID));
+    		}
+    	}
+    	
+    	/*
+    	 * Interpret GameKeyEvent.
+    	 * 3 different key profiles: n00b, Righty, Southpaw
+    	 */
+    	else if (event instanceof GameKeyEvent){
+    	   Player player = players.get(DUMMY_ID);
+    	   
+    	   switch(((GameKeyEvent)event).getKeyCode()){
+    	   		case KeyEvent.VK_UP:
+    	   		case KeyEvent.VK_W:
+    	   		case KeyEvent.VK_I: move(player, 0, -1); break;
+    	   		case KeyEvent.VK_LEFT:
+    	   		case KeyEvent.VK_A:
+    	   		case KeyEvent.VK_J: move(player, -1, 0); break;
+    	   		case KeyEvent.VK_DOWN:
+    	   		case KeyEvent.VK_S:
+    	   		case KeyEvent.VK_K: move(player, 0, 1); break;
+    	   		case KeyEvent.VK_RIGHT:
+    	   		case KeyEvent.VK_D:
+    	   		case KeyEvent.VK_L: move(player, 1, 0); break;
+    	   		case KeyEvent.VK_SPACE:
+    	   		case KeyEvent.VK_F:
+    	   		case KeyEvent.VK_SEMICOLON: bomb(player); break;
+    	   }
+       }
+    }
+    
+    private void move(Player player, int dx, int dy){
+    	// Get destination point
+    	Point point = grid.find(player);
+    	point.translate(dx, dy);
+    	
+    	// Move player if destination is passable.  Do nothing otherwise.
+    	if (grid.isPassable(point)){
+    		grid.set(player, point);
+    	}
+    }
+    
+    private void bomb(Player player){
+    	// TODO: implement
     }
 
+    public static void main(String[] args){
+		new Server();
+	}
 }
