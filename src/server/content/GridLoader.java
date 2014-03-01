f4687e2d358140068cff1386555b70a65d8b36bb
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-01 16:12:09 -0500

    GridLoader to append player numbers to players

--- src/server/content/GridLoader.java
+++ src/server/content/GridLoader.java
@@ -23,83 +23,92 @@ import common.models.Pillar;
 import common.models.Player;
 import common.models.Wall;
 
-public class GridLoader implements JsonDeserializer<Grid>{
+public class GridLoader {
 	
 	private static final String GRID_PATH = "grids/";
-	private static Gson gson;
+	private final Gson gson;
+	private final ClassLoader loader;
 	
-	public static Grid loadGrid(String gridFileName){
-		if (gson == null){
-			initialize();
-		}
-		
-		ClassLoader loader = GridLoader.class.getClassLoader();
+	public GridLoader(){
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Grid.class, new GridDeserializer());
+		gson = builder.serializeNulls().create();
+
+		loader = getClass().getClassLoader();
+	}
+	
+	public Grid load(String gridFileName){
 		InputStream s = loader.getResourceAsStream(GRID_PATH + gridFileName);
 		Reader reader = new InputStreamReader(s);
 		return gson.fromJson(reader, Grid.class);
 	}
 	
-	private static void initialize(){
-		GsonBuilder builder = new GsonBuilder();
-		builder.registerTypeAdapter(Grid.class, new GridLoader());
-		gson = builder.serializeNulls().create();
-	}
-
-	@Override
-	public Grid deserialize(
-		JsonElement ele, Type arg1, JsonDeserializationContext arg2
-	) throws JsonParseException {		
-		JsonObject gridObj = ele.getAsJsonObject();
-		
-		// Create grid
-		JsonObject sizeObj = gridObj.get("size").getAsJsonObject();
-		Grid grid = new Grid(
-			new Dimension(
-				sizeObj.get("width").getAsInt(),
-				sizeObj.get("height").getAsInt()
-			)
-		);
-		
-		JsonArray squares = gridObj.get("squares").getAsJsonArray();
-		for (JsonElement squareEle: squares){
-			if (!squareEle.isJsonNull()){
-				addSquare(grid, squareEle);
-			}
-		}
-		
-		return grid;
+	public static Grid loadGrid(String gridFileName){
+		GridLoader loader = new GridLoader();
+		return loader.load(gridFileName);
 	}
 	
-	private void addSquare(Grid grid, JsonElement squareEle){
-		JsonObject squareObj = squareEle.getAsJsonObject();
+	private class GridDeserializer implements JsonDeserializer<Grid>{
 		
-		// Get Point
-		JsonObject pointObj = squareObj.get("point").getAsJsonObject();
-		Point point = new Point(
-			pointObj.get("x").getAsInt(),
-			pointObj.get("y").getAsInt()
-		);
+		private int playerIndex;
 		
-		//Load entity and add to grid
-		for (JsonElement entityEle : squareObj.get("entities").getAsJsonArray()){
-			if (!entityEle.isJsonNull()){
-				grid.set(loadEntity(entityEle), point);
-			}
-		}
-	}
-	
-	private static Entity loadEntity(JsonElement entityEle){
-		String name = entityEle.getAsString();
-		
-		switch(name.toLowerCase()){
-			case "pillar": return new Pillar();
-			case "wall": return new Wall();
-			case "player": return new Player("Joe");
-			case "door": return new Door();
-			default: throw new IllegalArgumentException(
-				name + " is an illegal entity"
+		@Override
+		public Grid deserialize(
+			JsonElement ele, Type arg1, JsonDeserializationContext arg2
+		) throws JsonParseException {		
+			playerIndex = 1;
+			
+			JsonObject gridObj = ele.getAsJsonObject();
+			
+			// Create grid
+			JsonObject sizeObj = gridObj.get("size").getAsJsonObject();
+			Grid grid = new Grid(
+				new Dimension(
+					sizeObj.get("width").getAsInt(),
+					sizeObj.get("height").getAsInt()
+				)
 			);
+			
+			JsonArray squares = gridObj.get("squares").getAsJsonArray();
+			for (JsonElement squareEle: squares){
+				if (!squareEle.isJsonNull()){
+					addSquare(grid, squareEle);
+				}
+			}
+			
+			return grid;
+		}
+		
+		private void addSquare(Grid grid, JsonElement squareEle){
+			JsonObject squareObj = squareEle.getAsJsonObject();
+			
+			// Get Point
+			JsonObject pointObj = squareObj.get("point").getAsJsonObject();
+			Point point = new Point(
+				pointObj.get("x").getAsInt(),
+				pointObj.get("y").getAsInt()
+			);
+			
+			//Load entity and add to grid
+			for (JsonElement entityEle : squareObj.get("entities").getAsJsonArray()){
+				if (!entityEle.isJsonNull()){
+					grid.set(loadEntity(entityEle), point);
+				}
+			}
+		}
+		
+		private Entity loadEntity(JsonElement entityEle){
+			String name = entityEle.getAsString();
+			
+			switch(name.toLowerCase()){
+				case "pillar": return new Pillar();
+				case "wall": return new Wall();
+				case "player": return new Player("Player " + playerIndex++);
+				case "door": return new Door();
+				default: throw new IllegalArgumentException(
+					name + " is an illegal entity"
+				);
+			}
 		}
 	}
-
 }
