b7baba61e23a209d3a435ca2464ededd19f6c4e2
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-01 15:49:12 -0500

    Fully implement GridLoader

--- src/server/content/GridLoader.java
+++ src/server/content/GridLoader.java
@@ -1,6 +1,7 @@
 package server.content;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -8,12 +9,17 @@ import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import common.models.Entity;
 import common.models.Grid;
+import common.models.Pillar;
+import common.models.Player;
+import common.models.Wall;
 
 public class GridLoader implements JsonDeserializer<Grid>{
 	
@@ -40,13 +46,11 @@ public class GridLoader implements JsonDeserializer<Grid>{
 	@Override
 	public Grid deserialize(
 		JsonElement ele, Type arg1, JsonDeserializationContext arg2
-	) throws JsonParseException {
-		//JsonObject gridObj = new JsonObject();
-		
+	) throws JsonParseException {		
 		JsonObject gridObj = ele.getAsJsonObject();
 		
+		// Create grid
 		JsonObject sizeObj = gridObj.get("size").getAsJsonObject();
-		
 		Grid grid = new Grid(
 			new Dimension(
 				sizeObj.get("width").getAsInt(),
@@ -54,7 +58,45 @@ public class GridLoader implements JsonDeserializer<Grid>{
 			)
 		);
 		
+		JsonArray squares = gridObj.get("squares").getAsJsonArray();
+		for (JsonElement squareEle: squares){
+			if (!squareEle.isJsonNull()){
+				addSquare(grid, squareEle);
+			}
+		}
+		
 		return grid;
+	}
+	
+	private void addSquare(Grid grid, JsonElement squareEle){
+		JsonObject squareObj = squareEle.getAsJsonObject();
+		
+		// Get Point
+		JsonObject pointObj = squareObj.get("point").getAsJsonObject();
+		Point point = new Point(
+			pointObj.get("x").getAsInt(),
+			pointObj.get("y").getAsInt()
+		);
+		
+		//Load entity and add to grid
+		for (JsonElement entityEle : squareObj.get("entities").getAsJsonArray()){
+			if (!entityEle.isJsonNull()){
+				grid.set(loadEntity(entityEle), point);
+			}
+		}
+	}
+	
+	private static Entity loadEntity(JsonElement entityEle){
+		String name = entityEle.getAsString();
+		
+		switch(name.toLowerCase()){
+			case "pillar": return new Pillar();
+			case "wall": return new Wall();
+			case "player": return new Player("Joe");
+			default: throw new IllegalArgumentException(
+				name + " is an illegal entity"
+			);
+		}
 	}
 
 }
--- test/content/TestGridLoader.java
+++ test/content/TestGridLoader.java
@@ -3,19 +3,61 @@ package content;
 import static org.junit.Assert.*;
 
 import java.awt.Dimension;
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import common.models.Entity;
 import common.models.Grid;
-
+import common.models.Pillar;
+import common.models.Player;
+import common.models.Wall;
 import server.content.GridLoader;
 
 public class TestGridLoader {
+	
+	private Grid grid;
+	private final Set<Entity> checked = new HashSet<>();
+	
+	@Before
+	public void setUp(){
+		checked.clear();
+	}
 
 	@Test
 	public void test() {
-		Grid grid = GridLoader.loadGrid("grid1.json");
+		grid = GridLoader.loadGrid("grid1.json");
 		assertEquals(grid.getSize(), new Dimension(4, 4));
+		
+		check(1, 1, Pillar.class);
+		check(1, 2, Wall.class);
+		check(2, 2, Player.class);
+		check(3, 3, Pillar.class);
+		assertRemaining();
+	}
+	
+	private boolean check(int x, int y, Class<? extends Entity> type) {
+		for (Entity entity : grid.get(new Point(x, y))){
+			if (entity.getClass().equals(type)){
+				checked.add(entity);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void assertRemaining(){
+		for (Point point : grid.keySet()){
+			for (Entity entity : grid.get(point)){
+				assertTrue(
+					"Extra entity, " + entity.name + " was found on the grid.",
+					checked.contains(entity)
+				);
+			}
+		}
 	}
 
 }
