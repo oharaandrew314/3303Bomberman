600e894ccdd5cbfa5385a4ff8c10778d0a7a4195
Caleb Simpson <the.foner@gmail.com>
2014-03-02 09:48:30 -0500

    Fix potential issue with WinEvent class.

--- src/common/events/WinEvent.java
+++ src/common/events/WinEvent.java
@@ -5,9 +5,10 @@ import common.models.Player;
 public class WinEvent extends Event {
 
 	private static final long serialVersionUID = 4972263064235877315L;
-
-	public WinEvent(Player p) {
-		this.setPlayerID(p.playerId);
+        public final Player player;
+        
+	public WinEvent(Player player) {
+		this.player = player;
 	}
 
 }
