fcd7619c933c50bd94c09f425e1a48446e089db6
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-02 19:07:02 -0500

    Make view more data-driven

--- src/client/views/JFrameTextView.java
+++ src/client/views/JFrameTextView.java
@@ -1,6 +1,7 @@
 package client.views;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.TextArea;
 
@@ -11,21 +12,23 @@ import common.models.Player;
 
 public class JFrameTextView implements View {
 	
+	public static final Dimension FRAME_SIZE = new Dimension(760, 760);
+	public static final int GRID_TEXT_SIZE = 30;
 	private TextArea textArea, console;
 	
 	public JFrameTextView(){
 		JFrame frame = new JFrame("Bomberman");
 		frame.setLayout(new BorderLayout());
-		frame.setSize(760, 760);
+		frame.setSize(FRAME_SIZE);
 		
 		// Add Text Area
 		textArea = new TextArea();
-		textArea.setFont(new Font("monospaced", Font.PLAIN, 30));
+		textArea.setFont(new Font("monospaced", Font.PLAIN, GRID_TEXT_SIZE));
 		frame.add(textArea, BorderLayout.CENTER);
 		
 		// Add Console
 		console = new TextArea();
-		console.setSize(760, 80);
+		console.setSize(FRAME_SIZE.width, FRAME_SIZE.height / 10);
 		frame.add(console, BorderLayout.SOUTH);
 		
 		frame.setVisible(true);
