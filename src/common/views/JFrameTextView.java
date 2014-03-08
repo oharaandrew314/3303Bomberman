b6a92879b6aeb8f5007e708b2eee608885982104
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-07 21:46:09 -0500

    Remove explicit console size declaration
    BorderLayout handles the size nicely

--- src/common/views/JFrameTextView.java
+++ src/common/views/JFrameTextView.java
@@ -16,7 +16,6 @@ public abstract class JFrameTextView extends AbstractView {
 	
 	public static final Dimension FRAME_SIZE = new Dimension(760, 760);
 	public static final int GRID_TEXT_SIZE = 30;
-	public static final float CONSOLE_HEIGHT_PERCENT = (float) 0.1;
 	
 	public static final String LINE_SEP = System.getProperty("line.separator");
 	private TextArea textArea, console;
@@ -37,10 +36,6 @@ public abstract class JFrameTextView extends AbstractView {
 		
 		// Add Console
 		console = new TextArea();
-		console.setSize(
-			FRAME_SIZE.width, 
-			(int) (FRAME_SIZE.height / CONSOLE_HEIGHT_PERCENT)
-		);
 		console.setEditable(false);
 		frame.add(console, BorderLayout.SOUTH);
 		
