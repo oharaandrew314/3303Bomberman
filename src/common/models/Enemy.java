b83554609ef78391af233585c3bd6e9b1319110b
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-16 10:05:04 -0400

    Fix AI concurrency bug

--- src/common/models/Enemy.java
+++ src/common/models/Enemy.java
@@ -4,7 +4,18 @@ public class Enemy extends Unit {
 
 	private static final long serialVersionUID = 7031350047918428917L;
 	
+	private final long movementPeriod = 1200;
+	private long lastMovement;
+	
 	public Enemy(String name) {
 		super(name);
+	}
+	
+	public boolean isTimeToMove(long now){
+		if (now - lastMovement > movementPeriod){
+			lastMovement = now;
+			return true;
+		}
+		return false;
 	}
 }
