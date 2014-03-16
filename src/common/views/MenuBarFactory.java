feb4971cf8ed7f15f5a4a1cba4d04be83cd2daf9
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-16 11:33:01 -0400

    Add client controls help menu

--- src/common/views/MenuBarFactory.java
+++ src/common/views/MenuBarFactory.java
@@ -1,10 +1,12 @@
 package common.views;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 
 import server.controllers.Server;
 import server.views.LevelGeneratorDialog;
@@ -12,22 +14,36 @@ import server.views.LevelLoaderDialog;
 
 public class MenuBarFactory {
 	
-	public static JMenuBar createClientMenuBar(AbstractView view){
+	public static JMenuBar createClientMenuBar(JFrameTextView view){
 		JMenuBar menuBar = new JMenuBar();
 		
 		JMenu fileMenu = new JMenu("File");
+		fileMenu.add(new ExitAction(view));
+		
+		JMenu helpMenu = new JMenu("Help");
+		helpMenu.add(new ControlsAction(view.asComponent()));
+		
+		menuBar.add(fileMenu);
+		menuBar.add(helpMenu);
+		return menuBar;
+	}
+	
+	public static JMenuBar createServerMenuBar(Server server, JFrameTextView view){
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.add(new LoadGridAction(server));
+		fileMenu.add(new GenerateGridAction(server));
 		fileMenu.add(new ExitAction(view));
 		
 		menuBar.add(fileMenu);
 		return menuBar;
 	}
 	
-	public static JMenuBar createServerMenuBar(Server server, AbstractView view){
-		JMenuBar menuBar = new JMenuBar();
+	public static JMenuBar createSpectatorMenuBar(JFrameTextView view){
+JMenuBar menuBar = new JMenuBar();
 		
 		JMenu fileMenu = new JMenu("File");
-		fileMenu.add(new LoadGridAction(server));
-		fileMenu.add(new GenerateGridAction(server));
 		fileMenu.add(new ExitAction(view));
 		
 		menuBar.add(fileMenu);
@@ -81,5 +97,28 @@ public class MenuBarFactory {
 			new LevelGeneratorDialog(server);
 		}
 	}
+	
+	@SuppressWarnings("serial")
+	private static class ControlsAction extends AbstractAction {
+		
+		private final Component parent;
+		
+		public ControlsAction(Component parent){
+			super("Controls");
+			this.parent = parent;
+		}
 
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String controls = (
+				"Righty Controls:\n" +
+				"Movement: WASD, Bomb: F\n\n" +
+				"Southpaw Controls:\n" +
+				"Movement: IJKL, Bomb: ;\n\n" +
+				"n00b Controls:\n" +
+				"Movement: UpDownLeftRight, Bomb: Space"
+			);
+			JOptionPane.showMessageDialog(parent, controls);
+		}
+	}
 }
