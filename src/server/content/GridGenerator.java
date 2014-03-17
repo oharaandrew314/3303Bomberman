81e485b1f5615be05c439110e39f2d1c3d3a3727
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-03-17 01:00:55 -0400

    Generate grid with powerups and enemies
    powerups, enemies, and walls are distributed with a random function
    using density as an argument

--- src/server/content/GridGenerator.java
+++ src/server/content/GridGenerator.java
@@ -10,13 +10,25 @@ import common.models.Door;
 import common.models.Grid;
 import common.models.Pillar;
 import common.models.Wall;
+import common.models.powerups.BombPlusOnePowerup;
+import common.models.powerups.BombRangePowerup;
+import common.models.powerups.FlamePassPowerup;
+import common.models.powerups.MysteryPowerup;
+import common.models.powerups.Powerup;
+import common.models.units.Enemy;
+import common.models.units.LineEnemy;
+import common.models.units.PathFindingEnemy;
+import common.models.units.RandomEnemy;
 
 public class GridGenerator {
 	
 	private final Random r;
 	private final Grid grid;
 	
-	private static final double WALL_DENSITY = 0.7;
+	private static final double
+		WALL_DENSITY = 0.3,
+		ENEMY_DENSITY = 0.08,
+		POWERUP_DENSITY = 0.04;
 
 	private GridGenerator(Dimension size, Random r){
 		this.r = r;
@@ -24,30 +36,53 @@ public class GridGenerator {
 		
 		List<Point> points = new ArrayList<>(grid.keySet());
 		
-		// Create Pillars
+		// Create Pillars and walls
 		for (Point point : points){
 			if (grid.isPassable(point) && pillarLoc(point)){
 				grid.set(new Pillar(), point);
 			}
-		}
-		
-		// Create random walls
-		for (Point point : points){
-			if (chance(WALL_DENSITY) && grid.isPassable(point)){
+			else if (chance(WALL_DENSITY) && grid.isPassable(point)){
 				grid.set(new Wall(), point);
+			}
+			else if (chance(ENEMY_DENSITY) && grid.isPassable(point)){
+				grid.set(randomEnemy(r), point);
+			}
+			else if (chance(POWERUP_DENSITY) && grid.isPassable(point)){
+				grid.set(randomPowerup(r), point);
 			}
 		}
 		
-		// Place Door
+		// Place Door underneath a random wall
 		boolean placed = false;
 		while (!placed){
 			Point p = points.get(r.nextInt(points.size()));
-			placed = grid.set(new Door(), p);
+			if (grid.hasTypeAt(Wall.class, p)){
+				placed = grid.set(new Door(), p);
+			}
 		}
 	}
 	
-	private boolean chance(double chance){
-		return r.nextDouble() > chance;
+	private boolean chance(double density){
+		return r.nextDouble() < density;
+	}
+	
+	private Enemy randomEnemy(Random r){
+		switch(r.nextInt(3)){
+		case 0: return new RandomEnemy();
+		case 1: return new LineEnemy();
+		case 2: return new PathFindingEnemy();
+		default: throw new RuntimeException("Invalid random enemy");
+		}
+	}
+	
+	private Powerup randomPowerup(Random r){
+		switch(r.nextInt(4)){
+		case 0: return new BombPlusOnePowerup();
+		case 1: return new BombRangePowerup();
+		case 2: return new FlamePassPowerup();
+		case 3: return new MysteryPowerup();
+		default: throw new RuntimeException("Invalid random powerup");
+		}
 	}
 	
 	private boolean pillarLoc(Point point){
